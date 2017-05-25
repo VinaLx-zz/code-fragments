de525ce6bd7bc8cf2b6fb1698ae9a2825278e435
VinaLx <vinaleux@gmail.com>
2017-05-26 00:49:57 +0800

    finish copying control statement part of Kaleidoscope

--- llvm-tutorial/kaleidoscope.cpp
+++ llvm-tutorial/kaleidoscope.cpp
@@ -5,13 +5,16 @@
 #include <string>
 #include <utility>
 #include <vector>
-#include "llvm/ADT/APFloat.h"     // llvm::APFloat
-#include "llvm/IR/Function.h"     // llvm::Function
-#include "llvm/IR/IRBuilder.h"    // llvm::Value, llvm::IRBuilder
-#include "llvm/IR/LLVMContext.h"  // llvm::LLVMContext
-#include "llvm/IR/Module.h"       // llvm::Module
-#include "llvm/IR/Type.h"         // llvm::Type
-#include "llvm/IR/Verifier.h"     // llvm::VerifyFunction
+#include "llvm/ADT/APFloat.h"            // llvm::APFloat
+#include "llvm/IR/Function.h"            // llvm::Function
+#include "llvm/IR/IRBuilder.h"           // llvm::Value, llvm::IRBuilder
+#include "llvm/IR/LLVMContext.h"         // llvm::LLVMContext
+#include "llvm/IR/LegacyPassManager.h"   // llvm::legacy::FunctionPassManager
+#include "llvm/IR/Module.h"              // llvm::Module
+#include "llvm/IR/Type.h"                // llvm::Type
+#include "llvm/IR/Verifier.h"            // llvm::VerifyFunction
+#include "llvm/Transforms/Scalar.h"      // llvm::createReassociatePass
+#include "llvm/Transforms/Scalar/GVN.h"  // llvm::createGVNPass
 
 using namespace llvm;
 
@@ -20,7 +23,12 @@ enum Token {
     TOK_DEF = -2,
     TOK_EXTERN = -3,
     TOK_IDENTIFIER = -4,
-    TOK_NUMBER = -5
+    TOK_NUMBER = -5,
+    TOK_IF = -6,
+    TOK_THEN = -7,
+    TOK_ELSE = -8,
+    TOK_FOR = -9,
+    TOK_IN = -10
 };
 
 std::string gIdentifierStr;
@@ -42,6 +50,21 @@ int GetToken() {
         }
         if (gIdentifierStr == "extern") {
             return TOK_EXTERN;
+        }
+        if (gIdentifierStr == "if") {
+            return TOK_IF;
+        }
+        if (gIdentifierStr == "else") {
+            return TOK_ELSE;
+        }
+        if (gIdentifierStr == "then") {
+            return TOK_THEN;
+        }
+        if (gIdentifierStr == "for") {
+            return TOK_FOR;
+        }
+        if (gIdentifierStr == "in") {
+            return TOK_IN;
         }
         return TOK_IDENTIFIER;
     }
@@ -76,6 +99,20 @@ llvm::LLVMContext gContext;
 llvm::IRBuilder<> gBuilder(gContext);
 std::unique_ptr<llvm::Module> gModule;
 std::map<std::string, Value*> gNamedValues;
+
+// chapeter 4: optimization
+
+std::unique_ptr<llvm::legacy::FunctionPassManager> gFPM;
+
+void InitializeModuleAndPassManager() {
+    gModule = std::make_unique<llvm::Module>("my cool jit", gContext);
+    gFPM = std::make_unique<llvm::legacy::FunctionPassManager>(gModule.get());
+    gFPM->add(llvm::createInstructionCombiningPass());
+    gFPM->add(llvm::createReassociatePass());
+    gFPM->add(llvm::createGVNPass());
+    gFPM->add(llvm::createCFGSimplificationPass());
+    gFPM->doInitialization();
+}
 
 // parser
 
@@ -241,10 +278,132 @@ class FunctionAST {
         if (Value* ret_val = body_->CodeGen()) {
             gBuilder.CreateRet(ret_val);
             llvm::verifyFunction(*f);
+
+            gFPM->run(*f);
+
             return f;
         }
         f->eraseFromParent();
         return nullptr;
+    }
+};
+
+class IfExprAST : public ExprAST {
+    std::unique_ptr<ExprAST> cond_, then_, else_;
+
+  public:
+    IfExprAST(
+        std::unique_ptr<ExprAST> cond, std::unique_ptr<ExprAST> then,
+        std::unique_ptr<ExprAST> els)
+        : cond_(std::move(cond)),
+          then_(std::move(then)),
+          else_(std::move(els)) {}
+
+    Value* CodeGen() override {
+        Value* condv = cond_->CodeGen();
+        if (not condv)
+            return nullptr;
+        // On Not Equal
+        condv = gBuilder.CreateFCmpONE(
+            condv, ConstantFP::get(gContext, APFloat(0.0)), "ifcond");
+        Function* the_function = gBuilder.GetInsertBlock()->getParent();
+        BasicBlock* then_block =
+            BasicBlock::Create(gContext, "then", the_function);
+        BasicBlock* else_block = BasicBlock::Create(gContext, "else");
+        BasicBlock* merge_block = BasicBlock::Create(gContext, "ifcont");
+        gBuilder.CreateCondBr(condv, then_block, else_block);
+
+        gBuilder.SetInsertPoint(then_block);
+        Value* thenv = then_->CodeGen();
+        if (not thenv) {
+            return nullptr;
+        }
+        gBuilder.CreateBr(merge_block);
+        then_block = gBuilder.GetInsertBlock();
+
+        the_function->getBasicBlockList().push_back(else_block);
+        gBuilder.SetInsertPoint(else_block);
+        Value* elsev = else_->CodeGen();
+        if (not elsev) {
+            return nullptr;
+        }
+        gBuilder.CreateBr(merge_block);
+        else_block = gBuilder.GetInsertBlock();
+
+        the_function->getBasicBlockList().push_back(merge_block);
+        gBuilder.SetInsertPoint(merge_block);
+        llvm::PHINode* phi_node =
+            gBuilder.CreatePHI(Type::getDoubleTy(gContext), 2, "iftmp");
+        phi_node->addIncoming(thenv, then_block);
+        phi_node->addIncoming(elsev, else_block);
+        return phi_node;
+    }
+};
+
+class ForExprAST : public ExprAST {
+    std::string var_name_;
+    std::unique_ptr<ExprAST> start_, end_, step_, body_;
+
+  public:
+    ForExprAST(
+        const std::string& var_name, std::unique_ptr<ExprAST> start,
+        std::unique_ptr<ExprAST> end, std::unique_ptr<ExprAST> step,
+        std::unique_ptr<ExprAST> body)
+        : var_name_(var_name),
+          start_(std::move(start)),
+          step_(std::move(step)),
+          body_(std::move(body)) {}
+
+    Value* CodeGen() override {
+        // generate start value in original builder context
+        Value* startv = start_->CodeGen();
+        if (not startv) {
+            return nullptr;
+        }
+        Function* the_function = gBuilder.GetInsertBlock()->getParent();
+        // record pre_header_block
+        BasicBlock* pre_header_block = gBuilder.GetInsertBlock();
+        BasicBlock* loop_block =
+            BasicBlock::Create(gContext, "loop", the_function);
+        gBuilder.CreateBr(loop_block);
+        gBuilder.SetInsertPoint(loop_block);
+        PHINode* variable = gBuilder.CreatePHI(
+            Type::getDoubleTy(gContext), 2, var_name_.data());
+        variable->addIncoming(startv, pre_header_block);
+        // create an inner block for the loop
+        Value* oldval = gNamedValues[var_name_];
+        gNamedValues[var_name_] = variable;
+        if (not body_->CodeGen()) {
+            return nullptr;
+        }
+        Value* stepv = nullptr;
+        if (step_) {
+            stepv = step_->CodeGen();
+            if (not stepv) {
+                return nullptr;
+            }
+        } else {
+            stepv = ConstantFP::get(gContext, APFloat(1.0));
+        }
+        Value* next_value = gBuilder.CreateFAdd(variable, stepv, "nextvar");
+        Value* endv = end_->CodeGen();
+        if (not endv) {
+            return nullptr;
+        }
+        endv = gBuilder.CreateFCmpONE(
+            endv, ConstantFP::get(gContext, APFloat(0.0)), "loopcond");
+        BasicBlock* loop_end_block = gBuilder.GetInsertBlock();
+        BasicBlock* after_block =
+            BasicBlock::Create(gContext, "afterloop", the_function);
+        gBuilder.CreateCondBr(endv, loop_block, after_block);
+        gBuilder.SetInsertPoint(after_block);
+        variable->addIncoming(next_value, loop_end_block);
+        // restore context
+        if (oldval)
+            gNamedValues[var_name_] = oldval;
+        else
+            gNamedValues.erase(var_name_);
+        return Constant::getNullValue(Type::getDoubleTy(gContext));
     }
 };
 
@@ -259,6 +418,8 @@ std::unique_ptr<PrototypeAST> LogErrorP(const std::string& str) {
 }
 
 std::unique_ptr<ExprAST> ParseExpression();
+std::unique_ptr<ExprAST> ParseIfExpr();
+std::unique_ptr<ExprAST> ParseForExpr();
 
 /// numberexpr -> number
 std::unique_ptr<ExprAST> ParseNumberExpr() {
@@ -320,6 +481,10 @@ std::unique_ptr<ExprAST> ParsePrimary() {
             return ParseNumberExpr();
         case '(':
             return ParseParenExpr();
+        case TOK_IF:
+            return ParseIfExpr();
+        case TOK_FOR:
+            return ParseForExpr();
         default:
             return LogError("unknown token when expecting an expression");
     }
@@ -425,71 +590,137 @@ std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
     return nullptr;
 }
 
+/// ifexpr -> 'if' expression 'then' expression 'else' expression
+std::unique_ptr<ExprAST> ParseIfExpr() {
+    GetNextToken();
+    auto cond = ParseExpression();
+    if (not cond)
+        return nullptr;
+    if (gCurTok != TOK_THEN) {
+        return LogError("expect then");
+    }
+    GetNextToken();
+    auto then = ParseExpression();
+    if (not then) {
+        return nullptr;
+    }
+    if (gCurTok != TOK_ELSE) {
+        return LogError("expect else");
+    }
+    GetNextToken();
+    auto els = ParseExpression();
+    if (not els) {
+        return nullptr;
+    }
+    return std::make_unique<IfExprAST>(
+        std::move(cond), std::move(then), std::move(els));
+}
+
+std::unique_ptr<ExprAST> ParseForExpr() {
+    GetNextToken();
+    if (gCurTok != TOK_IDENTIFIER) {
+        return LogError("expected identifier after for");
+    }
+    std::string id_name(gIdentifierStr);
+    GetNextToken();
+    if (gCurTok != '=')
+        return LogError("expected '=' after for");
+    GetNextToken();
+    auto start = ParseExpression();
+    if (not start) {
+        return nullptr;
+    }
+    if (gCurTok != ',')
+        return LogError("expected ',' after for start value");
+    GetNextToken();
+    auto end = ParseExpression();
+    if (not end) {
+        return nullptr;
+    }
+    std::unique_ptr<ExprAST> step;
+    if (gCurTok == ',') {
+        GetNextToken();
+        step = ParseExpression();
+        if (not step)
+            return nullptr;
+    }
+    if (gCurTok != TOK_IN) {
+        return LogError("expect 'in' after for");
+    }
+    GetNextToken();
+    auto body = ParseExpression();
+    if (not body) {
+        return nullptr;
+    }
+    return std::make_unique<ForExprAST>(
+        id_name, std::move(start), std::move(end), std::move(step),
+        std::move(body));
+}
 //===----------------------------------------------------------------------===//
 // Top-Level parsing and JIT Driver
 //===----------------------------------------------------------------------===//
 
 static void HandleDefinition() {
-  if (auto FnAST = ParseDefinition()) {
-    if (auto *FnIR = FnAST->CodeGen()) {
-      fprintf(stderr, "Read function definition:");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    if (auto FnAST = ParseDefinition()) {
+        if (auto* FnIR = FnAST->CodeGen()) {
+            fprintf(stderr, "Read function definition:");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        // Skip token for error recovery.
+        GetNextToken();
     }
-  } else {
-    // Skip token for error recovery.
-    GetNextToken();
-  }
 }
 
 static void HandleExtern() {
-  if (auto ProtoAST = ParseExtern()) {
-    if (auto *FnIR = ProtoAST->CodeGen()) {
-      fprintf(stderr, "Read extern: ");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    if (auto ProtoAST = ParseExtern()) {
+        if (auto* FnIR = ProtoAST->CodeGen()) {
+            fprintf(stderr, "Read extern: ");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        // Skip token for error recovery.
+        GetNextToken();
     }
-  } else {
-    // Skip token for error recovery.
-    GetNextToken();
-  }
 }
 
 static void HandleTopLevelExpression() {
-  // Evaluate a top-level expression into an anonymous function.
-  if (auto FnAST = ParseTopLevelExpr()) {
-    if (auto *FnIR = FnAST->CodeGen()) {
-      fprintf(stderr, "Read top-level expression:");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    // Evaluate a top-level expression into an anonymous function.
+    if (auto FnAST = ParseTopLevelExpr()) {
+        if (auto* FnIR = FnAST->CodeGen()) {
+            fprintf(stderr, "Read top-level expression:");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        // Skip token for error recovery.
+        GetNextToken();
     }
-  } else {
-    // Skip token for error recovery.
-    GetNextToken();
-  }
 }
 
 /// top ::= definition | external | expression | ';'
 static void MainLoop() {
-  while (true) {
-    fprintf(stderr, "ready> ");
-    switch (gCurTok) {
-    case TOK_EOF:
-      return;
-    case ';': // ignore top-level semicolons.
-      GetNextToken();
-      break;
-    case TOK_DEF:
-      HandleDefinition();
-      break;
-    case TOK_EXTERN:
-      HandleExtern();
-      break;
-    default:
-      HandleTopLevelExpression();
-      break;
+    while (true) {
+        fprintf(stderr, "ready> ");
+        switch (gCurTok) {
+            case TOK_EOF:
+                return;
+            case ';':  // ignore top-level semicolons.
+                GetNextToken();
+                break;
+            case TOK_DEF:
+                HandleDefinition();
+                break;
+            case TOK_EXTERN:
+                HandleExtern();
+                break;
+            default:
+                HandleTopLevelExpression();
+                break;
+        }
     }
-  }
 }
 
 //===----------------------------------------------------------------------===//
@@ -497,25 +728,24 @@ static void MainLoop() {
 //===----------------------------------------------------------------------===//
 
 int main() {
-  // Install standard binary operators.
-  // 1 is lowest precedence.
-  gBinopPrecedence['<'] = 10;
-  gBinopPrecedence['+'] = 20;
-  gBinopPrecedence['-'] = 20;
-  gBinopPrecedence['*'] = 40; // highest.
+    // Install standard binary operators.
+    // 1 is lowest precedence.
+    gBinopPrecedence['<'] = 10;
+    gBinopPrecedence['+'] = 20;
+    gBinopPrecedence['-'] = 20;
+    gBinopPrecedence['*'] = 40;  // highest.
 
-  // Prime the first token.
-  fprintf(stderr, "ready> ");
-  GetNextToken();
+    // Prime the first token.
+    fprintf(stderr, "ready> ");
+    GetNextToken();
 
-  // Make the module, which holds all the code.
-  gModule = llvm::make_unique<Module>("my cool jit", gContext);
+    InitializeModuleAndPassManager();
 
-  // Run the main "interpreter loop" now.
-  MainLoop();
+    // Run the main "interpreter loop" now.
+    MainLoop();
 
-  // Print out all of the generated code.
-  gModule->print(errs(), nullptr);
+    // Print out all of the generated code.
+    gModule->print(errs(), nullptr);
 
-  return 0;
+    return 0;
 }
